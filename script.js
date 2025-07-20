6e84a909827b2f74f42974b1ac8d1ae2901f17bf
Sneha BV <152629103+Sn01eha@users.noreply.github.com>
2025-07-20 08:59:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Homepage script loaded!");
+    // You can add interactive elements here later if needed.
+    // For example, if you wanted to add a "scroll to top" button
+    // or dynamic content loading.
+});
+
+// Example of a simple function you might add later:
+function greetUser() {
+    alert("Welcome to Sneha's Portfolio!");
+}
+
+// You could call this function on a button click, for instance:
+// document.querySelector('.some-button').addEventListener('click', greetUser);
